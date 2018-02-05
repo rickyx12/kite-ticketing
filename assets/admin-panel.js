b73a7935d1f1ad864ca313fc1260a177b0c0db82
Ricardo Osit <aizekrysler@gmail.com>
2018-02-05 14:41:10 +0800

    js for admin UI

---
+++ assets/admin-panel.js
@@ -0,0 +1,67 @@
+function getPublishedTicket(json) {
+	$.getJSON(json,'',function(result) {
+		var tableData = '';
+		console.log(result);
+
+		if(result != "null") {
+			$.each(result,function(i,field) {	
+				 tableData += '<tr>';
+				 tableData += '<td>'+field.ticketNo+'</td>';
+				 tableData += '<td>'+field.date+'</td>';
+				 tableData += '<td>'+field.employee+'</td>'
+				 tableData += '<td><form id="ticket-form'+field.ticketNo+'" action="ticket-form.php" method="post" target="_blank" style="margin-bottom:0px;"><input type="hidden" name="ticketNo" value="'+field.ticketNo+'"><div class="btn-group"><button id="view-published-btn'+field.ticketNo+'" class="btn btn-default">View</button></div></form></td>';
+				 tableData += '</tr>';
+
+				 $(document).on('click','#view-published-btn'+field.ticketNo,function(event){
+				 	$('#ticket-form'+field.ticketNo).submit();
+				 });
+
+			});
+		}else {
+			tableData += '<tr></tr>';
+		}
+
+		 $('#admin-published-ticket-table').html(tableData);
+	});
+}
+
+function getTodayPublishedTicket(json) {
+	$.getJSON(json,'',function(result) {
+		var tableData = '';
+		console.log(result);
+
+		if(result != "null") {
+			$.each(result,function(i,field) {	
+				 tableData += '<tr>';
+				 tableData += '<td>'+field.ticketNo+'</td>';
+				 tableData += '<td>'+field.date+'</td>';
+				 tableData += '<td>'+field.employee+'</td>'
+				 tableData += '<td><form id="ticket-form'+field.ticketNo+'" action="ticket-form.php" method="post" target="_blank" style="margin-bottom:0px;"><input type="hidden" name="ticketNo" value="'+field.ticketNo+'"><div class="btn-group"><button id="view-published-btn'+field.ticketNo+'" class="btn btn-default">View</button></div></form></td>';
+				 tableData += '</tr>';
+
+				 $(document).on('click','#view-published-btn'+field.ticketNo,function(event){
+				 	$('#ticket-form'+field.ticketNo).submit();
+				 });
+
+			});
+		}else {
+			tableData += '<tr></tr>';
+		}
+
+		 $('#admin-published-ticket-today').html(tableData);
+	});
+}
+
+$(function(){
+
+	setTimeout(function(){
+		getTodayPublishedTicket('today-published-json-encoder.php');
+		getPublishedTicket('published-ticket-json-encoder.php');
+	},10000);
+
+	$('#search-ticket').keyup(function(){
+		var search = $('#search-ticket').val();
+		getPublishedTicket('search-published-ticket-json-encoder.php?search='+search);
+	});
+
+});
