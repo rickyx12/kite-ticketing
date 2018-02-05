56a8a0e2cf6c4d3ec15cc276e256fc6ca7f375fb
Ricardo Osit <aizekrysler@gmail.com>
2018-02-05 16:24:20 +0800

    added today tab for todays published ticket

--- views/admin-panel.php
+++ views/admin-panel.php
@@ -8,18 +8,41 @@
 		<script src="../assets/jquery-3.3.1.min.js"></script>
 		<link rel="stylesheet" type="text/css" href="../assets/bootstrap-3.3.7-dist/css/bootstrap.min.css">
 		<script src="../assets/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>		
+		<script src="../assets/admin-panel.js"></script>
 	</head>
 	<?php include 'navbar.php' ?>
 	<body>
 		<div class="container">
 		  <!-- Nav tabs -->
 		  <ul class="nav nav-tabs" role="tablist">
-		    <li role="presentation" class="active"><a href="#published" aria-controls="home" role="tab" data-toggle="tab">Published</a></li>
+		  	<li role="presentation" class="active"><a href="#today" aria-controls="home" role="tab" data-toggle="tab">Today</a></li>
+		    <li role="presentation"><a href="#published" aria-controls="home" role="tab" data-toggle="tab">Published</a></li>
 		  </ul>
 
 		  <!-- Tab panes -->
 		  <div class="tab-content">
-		    <div role="tabpanel" class="tab-pane active" id="published">
+
+		  	<div role="tabpanel" class="tab-pane active" id="today">
+		  		<br>
+		  		<div class="row">
+		  			<div class="col-md-6">
+		  				<table class="table table-bordered">
+		  					<thead>
+		  						<tr>
+		  							<th>Ticket#</th>
+		  							<th>Date</th>
+		  							<th>Employee</th>
+		  							<th></th>
+		  						</tr>
+		  					</thead>
+		  					<tbody id="admin-published-ticket-today">
+		  					</tbody>
+		  				</table>
+		  			</div>
+		  		</div>
+		  	</div>
+
+		    <div role="tabpanel" class="tab-pane" id="published">
 		    	<br>
 		    	<div class="row">
 		    		<div class="col-md-4">
@@ -40,9 +63,10 @@
 				    				<th>Ticket#</th>
 				    				<th>Date</th>
 				    				<th>Employee</th>
+				    				<th></th>
 				    			</tr>
 				    		</thead>
-				    		<tbody id="admin-published-ticket">
+				    		<tbody id="admin-published-ticket-table">
 				    		</tbody>
 				    	</table>
 			    	</div>
